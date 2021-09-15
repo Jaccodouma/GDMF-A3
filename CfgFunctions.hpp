5292de7f638474f50626d0db4a376360742d60e7
Jacco Douma <jaccodouma@gmail.com>
2021-09-15 10:40:21 +0200

    Create CfgFunctions.hpp

---
+++ CfgFunctions.hpp
@@ -0,0 +1,20 @@
+class CfgFunctions 
+{
+	class GD 
+	{
+		class Mission
+		{
+			// Add your mission functions here
+		};
+	};
+
+	// Don't touch these
+	class GDMF 
+	{
+		class GDMF
+		{
+			class init {};
+			class scaleDamage {}; 
+		}
+	}
+};
