25a4ba0803133d7d48e6297856ee69b30912ece8
Jacco Douma <jaccodouma@gmail.com>
2021-09-18 19:03:33 +0200

    Update spectate.hpp

--- respawn/spectate.hpp
+++ respawn/spectate.hpp
@@ -1,4 +1,11 @@
 GD_RespawnType = 0; 
 
-respawn = 1; // No respawn 
+respawn = 3;
+respawnButton = 0; 
+
+respawnDelay = 14400; // 4 hours? x) 
+respawnVehicleDelay = 120; // In case you use this
+respawnDialog = 0; 
+respawnOnStart = 0; 
+
 respawnTemplates[] = {"ace_spectator"}; 
